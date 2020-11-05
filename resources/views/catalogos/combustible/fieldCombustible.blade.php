7fe6ab7d8019052518015eff97c90cf5f0e916e3
Itihell <itihell.mejia@gmail.com>
2020-11-05 09:37:54 -0600

    instalacion de vue-select y campos con formato

--- resources/views/catalogos/combustible/fieldCombustible.blade.php
+++ resources/views/catalogos/combustible/fieldCombustible.blade.php
@@ -1,3 +1,9 @@
 {!! Form::token() !!}
-{{ Form::label('Combustible', null, ['class' => 'control-label']) }}
-{!! Form::text('combustible',null,$attributes=['class' => 'form-control']) !!}
+
+
+
+<div class="form-group">
+    <label for="exampleInputEmail1">Combustible</label>
+    {!! Form::text('combustible',null,$attributes=['class' => 'form-control']) !!}
+    <small id="emailHelp" class="form-text text-muted">Escriba el nombre del combustible</small>
+</div>
--- resources/views/catalogos/combustible/index.blade.php
+++ resources/views/catalogos/combustible/index.blade.php
@@ -1,38 +1,38 @@
 @extends('layouts.app')
 @section('content')
-    <a href="{{ route('combustible.add') }}" class="btn btn-info btn-sm">
-        Nuevo Combustible
-    </a>
-    <div class="table-responsive">
-        <table class="table table-sm">
-            <thead>
+<a href="{{ route('combustible.add') }}" class="btn btn-info btn-sm">
+    Nuevo Combustible
+</a>
+<div class="table-responsive">
+    <table class="table table-sm">
+        <thead>
             <tr class="bg-dark text-white">
-                <th width="80px" >Código</th>
+                <th width="80px">Código</th>
                 <th>Combustible</th>
                 <th width="100px">Acción</th>
             </tr>
-            </thead>
-            <tbody>
+        </thead>
+        <tbody>
             @foreach($rows as $row)
-                <tr>
-                    <td>{{ $row->id  }}</td>
-                    <td>{{ $row->combustible  }}</td>
-                    <td>
-<div class="btn-group-sm">
-    {!! Form::open(['route' => ['combustible.delete',$row->id],'method' => 'delete']) !!}
-    {!! Form::token() !!}
-    <button type="submit" class="btn btn-danger btn-sm">
-        Eliminar
-    </button>
-    {!! Form::close() !!}
-    <a href="{{ route('combustible.edit',$row->id) }}" class="btn btn-danger btn-sm">
-        Editar
-    </a>
-</div>
-                    </td>
-                </tr>
+            <tr>
+                <td>{{ $row->id  }}</td>
+                <td>{{ $row->combustible  }}</td>
+                <td>
+                    <div class="btn-group-sm">
+                        {!! Form::open(['route' => ['combustible.delete',$row->id],'method' => 'delete']) !!}
+                        {!! Form::token() !!}
+                        <button type="submit" class="btn btn-danger btn-sm">
+                            Eliminar
+                        </button>
+                        {!! Form::close() !!}
+                        <a href="{{ route('combustible.edit',$row->id) }}" class="btn btn-danger btn-sm">
+                            Editar
+                        </a>
+                    </div>
+                </td>
+            </tr>
             @endforeach
-            </tbody>
-        </table>
-    </div>
+        </tbody>
+    </table>
+</div>
 @endsection
